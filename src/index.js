13a5f6274f850ee90fd053aa629dfa58d782cb3d
Alex Kim <alexkim251@yahoo.com>
2021-06-12 16:12:51 -0500

    added reduxStore and provider to index.js about to add reducers

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+// added 3 imports
+import {Provider } from 'react-redux'
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+const reduxStore = createStore(
+    combineReducers({
+        feeling,understanding,support,comments
+    }),
+    applyMiddleware(logger)
+);
+
+ReactDOM.render(<Provider store={reduxStore}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
