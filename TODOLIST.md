45bf3b0f310f2c8039e144a0846aabfcf8101f8e
Alex Kim <alexkim251@yahoo.com>
2021-06-12 19:48:14 -0500

    added radio button inputs and button for submit runs a addfeeling function which dispatches to the reducer and history pushes to next page

--- TODOLIST.md
+++ TODOLIST.md
@@ -10,44 +10,45 @@ Inital Setup
     [x] set up redux and provider
     [x] set up reducers (one for each component?)
 Components
-    [ ] App.js
-        [ ] components import and setup component files
+    [x] App.js
+        [x] components import and setup component files
         [x] routes to components
     [ ] Feeling page 1
         [x] import export
         [ ] dispatch/submit to reducer
         [ ] handlevaluechange
         [ ] input validation
-        [ ] return
+        [ ] return/render
     [ ] Understanding page 2
         [x] import export
         [ ] dispatch/submit to reducer
         [ ] handlevaluechange
         [ ] input validation
-        [ ] return
+        [ ] return/render
     [ ] Support 3
         [x] import export
         [ ] dispatch/submit to reducer
         [ ] handlevaluechange
         [ ] input validation
         [ ] route change
-        [ ] return
+        [ ] return/render
     [ ] comments 4
         [x] import export
         [ ] dispatch/submit to reducer
         [ ] handlevaluechange
-        [ ] return
+        [ ] return/render
         [ ] route change
     [x] Review and Submit 5
         [x] import export
         [x] dispatch/submit to database
         [x] handlevaluechange
-        [x] return
+        [x] return/render
         [x] route change
+        -(still review this page use selectors might be incorrect)
     [ ] Reset page 6
-        [ ] import export
+        [x] import export
         [ ] dispatch reset submit call
-        [ ] return
+        [ ] return/render
         [ ] route change to page 1
         [ ] reset values in reducers
 CSS
--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,13 +1,93 @@
 import { useHistory } from 'react-router';
-import {useDispatch} from 'react-redux'
+import {useDispatch} from 'react-redux';
+import  { useState } from "react";
 
 function Feeling() {
     // make use dispatch accessible through 'dispatch'
     const dispatch = useDispatch();
+    // make history accessible through 'history'
+    const history = useHistory();
+
+    //manages state of feeling variable
+    let [feeling, setFeeling] =useState('')
+
+    //handles state change of radio input
+    const handleChange = (event) => {
+
+        setFeeling(
+            event.target.value,
+        )
+    }
+
+    const addFeeling = (event) => {
+        event.preventDefault();
+        
+        dispatch({
+            type: 'ADD_FEELING',
+            payload: feeling
+        })
+
+        history.push('/understanding')
+    }
 
     return (
         <>
-            feeling
+            <form>
+                <div>
+                    How are you doing today?
+                    <br/>
+                    <div onChange={handleChange}>
+                    <label>
+                    <input 
+                    
+                    name='value'
+                    type='radio'
+                    value='1'
+                    defaultChecked
+                    />
+                    1
+                    </label>
+                    <label>
+                    <input 
+                    
+                    name='value'
+                    type='radio'
+                    value='2'
+                    />
+                    2
+                    </label>
+                    <label>
+                    <input 
+                    
+                    name='value'
+                    type='radio'
+                    value='3'
+                    />
+                    3
+                    </label>
+                    <label>
+                    <input 
+                    
+                    name='value'
+                    type='radio'
+                    value='4'
+                    />
+                    4
+                    </label>
+                    <label>
+                    <input 
+                    
+                    name='value'
+                    type='radio'
+                    value='5'
+                    />
+                    5
+                    </label>
+                    </div>
+                    <br/>
+                    <button onClick={addFeeling}>Next</button>
+                </div>
+            </form>
         </>
     )
 
