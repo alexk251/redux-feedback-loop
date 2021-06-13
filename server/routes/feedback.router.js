f0794e45512c32bd28337d672c97c21ee2ad52b0
Alex Kim <alexkim251@yahoo.com>
2021-06-13 11:45:54 -0500

    added delete router in server side

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -41,4 +41,14 @@ router.post('/', async (req, res) => {
     }
 });
 
+// DELETE an order
+router.delete('/:id', (req, res) => {
+    pool.query('DELETE FROM "feedback" WHERE id=$1', [req.params.id]).then((result) => {
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log('Error DELETE /feedback', error);
+        res.sendStatus(500);
+    })
+});
+
 module.exports = router;
