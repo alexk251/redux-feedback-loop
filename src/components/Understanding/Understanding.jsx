7da6e07597fb25160e638811973a3d906d088b90
Alex Kim <alexkim251@yahoo.com>
2021-06-12 19:54:34 -0500

    mirrored setup for feeling component onto Understanding component but switched out variables

--- src/components/Understanding/Understanding.jsx
+++ src/components/Understanding/Understanding.jsx
@@ -1,13 +1,93 @@
 import { useHistory } from 'react-router';
-import {useDispatch} from 'react-redux'
+import {useDispatch} from 'react-redux';
+import  { useState } from "react";
 
 function Understanding() {
     // make use dispatch accessible through 'dispatch'
     const dispatch = useDispatch();
+    // make history accessible through 'history'
+    const history = useHistory();
+
+    //manages state of feeling variable
+    let [understanding, setUnderstanding] =useState('1')
+
+    //handles state change of radio input
+    const handleChange = (event) => {
+
+        setUnderstanding(
+            event.target.value,
+        )
+    }
+
+    const addUnderstanding = (event) => {
+        event.preventDefault();
+        
+        dispatch({
+            type: 'ADD_UNDERSTANDING',
+            payload: understanding
+        })
+
+        history.push('/support')
+    }
 
     return (
         <>
-            Understanding
+            <form>
+                <div>
+                    How are you doing today?
+                    <br/>
+                    <div onChange={handleChange}>
+                    <label>
+                    <input 
+                    
+                    name='value'
+                    type='radio'
+                    value='1'
+                    defaultChecked
+                    />
+                    1
+                    </label>
+                    <label>
+                    <input 
+                    
+                    name='value'
+                    type='radio'
+                    value='2'
+                    />
+                    2
+                    </label>
+                    <label>
+                    <input 
+                    
+                    name='value'
+                    type='radio'
+                    value='3'
+                    />
+                    3
+                    </label>
+                    <label>
+                    <input 
+                    
+                    name='value'
+                    type='radio'
+                    value='4'
+                    />
+                    4
+                    </label>
+                    <label>
+                    <input 
+                    
+                    name='value'
+                    type='radio'
+                    value='5'
+                    />
+                    5
+                    </label>
+                    </div>
+                    <br/>
+                    <button onClick={addUnderstanding}>Next</button>
+                </div>
+            </form>
         </>
     )
 
