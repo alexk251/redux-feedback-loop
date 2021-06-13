cb6e1c3563d503f025a6f6e3f6ba62934c032ddb
Alex Kim <alexkim251@yahoo.com>
2021-06-12 19:59:56 -0500

    took feeling component and mirror and swapped out variables on Support component

--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -1,13 +1,94 @@
 import { useHistory } from 'react-router';
-import {useDispatch} from 'react-redux'
+import {useDispatch} from 'react-redux';
+import  { useState } from "react";
 
 function Support() {
     // make use dispatch accessible through 'dispatch'
     const dispatch = useDispatch();
+    // make history accessible through 'history'
+    const history = useHistory();
+
+    //manages state of support variable
+    let [support, setSupport] =useState('1')
+
+    //handles state change of radio input
+    const handleChange = (event) => {
+
+        setSupport(
+            event.target.value,
+        )
+    }
+
+    //handles next button and dispatch & page change
+    const addSupport = (event) => {
+        event.preventDefault();
+        
+        dispatch({
+            type: 'ADD_SUPPORT',
+            payload: support
+        })
+
+        history.push('/understanding')
+    }
 
     return (
         <>
-            Support
+            <form>
+                <div>
+                    How are you doing today?
+                    <br/>
+                    <div onChange={handleChange}>
+                    <label>
+                    <input 
+                    
+                    name='value'
+                    type='radio'
+                    value='1'
+                    defaultChecked
+                    />
+                    1
+                    </label>
+                    <label>
+                    <input 
+                    
+                    name='value'
+                    type='radio'
+                    value='2'
+                    />
+                    2
+                    </label>
+                    <label>
+                    <input 
+                    
+                    name='value'
+                    type='radio'
+                    value='3'
+                    />
+                    3
+                    </label>
+                    <label>
+                    <input 
+                    
+                    name='value'
+                    type='radio'
+                    value='4'
+                    />
+                    4
+                    </label>
+                    <label>
+                    <input 
+                    
+                    name='value'
+                    type='radio'
+                    value='5'
+                    />
+                    5
+                    </label>
+                    </div>
+                    <br/>
+                    <button onClick={addSupport}>Next</button>
+                </div>
+            </form>
         </>
     )
 
